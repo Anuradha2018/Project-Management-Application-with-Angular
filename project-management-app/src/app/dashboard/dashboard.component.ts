4436e55498d65ad34da7c1f190f1bf35f38b670d
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-10-21 09:42:19 +0200

    dasboard component
    Credit: Material dashboard

--- project-management-app/src/app/dashboard/dashboard.component.ts
+++ project-management-app/src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
+
 
 }
