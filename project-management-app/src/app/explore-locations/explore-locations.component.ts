2595cfc67e7d5c8f26595f9bc7ecf7738798ff5c
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-10-28 22:37:08 +0100

    a test view for explore-location

--- project-management-app/src/app/explore-locations/explore-locations.component.ts
+++ project-management-app/src/app/explore-locations/explore-locations.component.ts
@@ -1,7 +1,12 @@
+import { Technology } from './../models/technology';
+import { ExploreTeamService } from './../services/explore-team.service';
+import { ExploreProjectService } from './../services/explore-project.service';
 import { ExploreLocationService } from './../services/explore-location.service';
+
 import { ExploreTeam } from './../models/explore-team';
 import { ExploreProject } from './../models/explore-project';
 import { Location} from './../models/explore-location';
+
 import { Component, OnInit } from '@angular/core';
 import { ExploreTeamsComponent } from './../explore-teams/explore-teams.component';
 import { ExploreProjectsComponent } from './../explore-projects/explore-projects.component';
