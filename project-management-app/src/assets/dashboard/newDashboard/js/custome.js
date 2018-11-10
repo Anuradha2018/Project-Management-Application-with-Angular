339fb58e3417ff5258147ff7fcc73abb1ed1c9bc
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-11-10 13:01:00 +0100

    assets required for new dashboard added

---
+++ project-management-app/src/assets/dashboard/newDashboard/js/custome.js
@@ -0,0 +1,74 @@
+$.fn.extend({
+    treed: function (o) {
+      
+      var openedClass = 'fa-minus-circle';
+      var closedClass = 'fa-plus-circle';
+      
+      if (typeof o != 'undefined'){
+        if (typeof o.openedClass != 'undefined'){
+        openedClass = o.openedClass;
+        }
+        if (typeof o.closedClass != 'undefined'){
+        closedClass = o.closedClass;
+        }
+      };
+      
+        //initialize each of the top levels
+        var tree = $(this);
+        tree.addClass("tree");
+        
+        tree.find('li').has("ul").each(function () {
+            var branch = $(this); //li with children ul
+            branch.prepend("<i class='indicator fa " + closedClass + "'></i>");
+            branch.addClass('branch');
+            branch.on('click', function (e) {
+                if (this == e.target) {
+                     var icon = $(this).children('i:first');
+                    icon.toggleClass(openedClass + " " + closedClass);
+                    
+                   // $(this).addClass('dote');
+                    $(this).children().children().toggle();
+                    $(this).addClass('dote');
+                   
+                }
+            })
+            branch.children().children().toggle();
+           
+            
+           
+           
+        });
+        //fire event from the dynamically added icon
+      tree.find('.branch span').each(function(){
+        $(this).on('click', function () {
+            $(this).closest('li').click();
+            tree.find('span').removeClass('actives');
+            $(this).addClass('actives');
+        });
+      });
+        //fire event to open branch if the li contains an anchor instead of text
+        tree.find('.branch>a').each(function () {
+            $(this).on('click', function (e) {
+                $(this).closest('li').click();
+                e.preventDefault();
+                 
+            });
+        });
+        //fire event to open branch if the li contains a button instead of text
+        tree.find('.branch>button').each(function () {
+            $(this).on('click', function (e) {
+                $(this).closest('li').click();
+                e.preventDefault();
+
+            });
+        });
+    }
+});
+
+//Initialization of treeviews
+
+$('#tree1').treed();
+
+$('#tree2').treed({openedClass:'glyphicon-folder-open', closedClass:'glyphicon-folder-close'});
+
+$('#tree3').treed({openedClass:'glyphicon-chevron-right', closedClass:'glyphicon-chevron-down'});
